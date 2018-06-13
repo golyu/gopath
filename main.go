c70d27ce7cdeffc9733016450b747f479fbe1ee2
吕中宜 <lvzhongyiforchrome@gmail.com>
2018-06-13 20:17:51 +0800

    feat(main):可以代理执行其他命令

--- main.go
+++ main.go
@@ -6,6 +6,8 @@ import (
 	"flag"
 	"strings"
 	"fmt"
+	"os/exec"
+	"log"
 )
 
 var (
@@ -14,22 +16,29 @@ var (
 )
 
 func main() {
-	flag.StringVar(&curPath, "path", "", "要设置为动态path的路径,不设置则为当前路径,如果是在src下,就设置为src的上级目录")
-	if curPath == "" {
-		// 当前路径
-		pwd, _ := os.Getwd()
-		curPath = pwd
-		if strings.HasSuffix(pwd, "/src") {
-			curPath = strings.TrimSuffix(pwd, "/src")
-		}
-		// 上级目录,也就是如果你在 /User/aaa/bbb/ccc/src/ddd下执行,我也会把/User/aaa/bbb/ccc设置为环境变量
-		upPath := strings.TrimSuffix(pwd, "/"+filepath.Base(pwd))
-		if strings.HasSuffix(upPath, "/src") {
-			curPath = strings.TrimSuffix(upPath, "/src")
-		}
-
+	//flag.StringVar(&curPath, "path", "", "要设置为动态path的路径,不设置则为当前路径,如果是在src下,就设置为src的上级目录")
+	//if curPath == "" {
+	// 当前路径
+	pwd, _ := os.Getwd()
+	curPath = pwd
+	if strings.HasSuffix(pwd, "/src") {
+		curPath = strings.TrimSuffix(pwd, "/src")
 	}
+	// 上级目录,也就是如果你在 /User/aaa/bbb/ccc/src/ddd下执行,我也会把/User/aaa/bbb/ccc设置为环境变量
+	upPath := strings.TrimSuffix(pwd, "/"+filepath.Base(pwd))
+	if strings.HasSuffix(upPath, "/src") {
+		curPath = strings.TrimSuffix(upPath, "/src")
+	}
+
+	//}
+	// 设置GoPath
 	ensureGoPath()
+
+	// 对应命令执行
+	cmds := flag.Args()
+	if len(cmds) > 2 {
+		runPrint(cmds[1], cmds[1:])
+	}
 }
 
 // ensureGoPath 配置 GoPath
@@ -49,4 +58,16 @@ func ensureGoPath() {
 	} else {
 		fmt.Println("当前命令行的临时环境变量为:", fullPath)
 	}
+}
+
+// runPrint 执行命令
+func runPrint(cmd string, args []string) {
+	eCmd := exec.Command(cmd, args...)
+	eCmd.Stdout = os.Stdout
+	eCmd.Stderr = os.Stderr
+	err := eCmd.Run()
+	if err != nil {
+		log.Fatal("执行命令发生错误:", err.Error())
+	}
+
 }
